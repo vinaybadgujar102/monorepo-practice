41678f82d6f7e2e5b98168ad0d65ea20b5b3463f
Vinay Arvind Badgujar <90821253+vinaybadgujar102@users.noreply.github.com>
2023-08-21 11:17:55 +0530

    initial commit

---
+++ apps/client/src/pages/signup.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Signup from 'ui/components/Signup';
+
+const signup = () => {
+  return (
+    <div>
+      <Signup />
+    </div>
+  )
+}
+
+export default signup;
---
+++ packages/ui/components/Signup.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      <input type='text' placeholder='email'></input>
+      <input type='text' placeholder='password'></input>
+      <button>Submit</button>
+    </div>
+  )
+}
+
+export default Signup
