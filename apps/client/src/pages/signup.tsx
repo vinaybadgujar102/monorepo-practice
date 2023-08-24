3213786533b85a0cf38718e0affc5162e01fb4d9
Vinay Arvind Badgujar <90821253+vinaybadgujar102@users.noreply.github.com>
2023-08-24 20:03:07 +0530

    cp 3: api complete

--- apps/client/src/pages/signup.tsx
+++ apps/client/src/pages/signup.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Signup from 'ui/components/Signup';
+import axios from 'axios';
 
 const signup = () => {
   return (
     <div>
-      <Signup onClick = {(username, password) => {
-        alert(username);
-        alert(password);
+      <Signup onClick = {async (username, password) => {
+        const response = await axios.post("/api/signup", {
+          username,
+          password
+        })
+        localStorage.setItem("token", response.data.token)
       }}/>
     </div>
   )
---
+++ packages/store/src/index.ts
@@ -0,0 +1,5 @@
+export * from './atoms/course'
+export * from './atoms/user'
+export * from './selectors/course'
+export * from './selectors/userEmail'
+export * from './selectors/isUserLoading'
